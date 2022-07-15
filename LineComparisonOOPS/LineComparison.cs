8734f4a0838684001255f274504bcc36fd182ee5
Megha8150 <meghananayak436@gmail.com>
2022-07-16 00:22:16 +0530

    Calculated length

--- LineComparisonOOPS/LineComparison.cs
+++ LineComparisonOOPS/LineComparison.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 namespace LineComparisonOOPS
 {
     public class LineComparison
+
     {
-        public static void lineComparison()
+        int x1,x2,x3,x4;
+        public double length()
+
         {
-            Console.WriteLine("Welcome to Line Comparison");
+            Console.WriteLine("Enter the value of x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            return Math.Sqrt((Math.Pow((x2 - x1), 2) + ((Math.Pow((y2 - y1), 2)))));
+
         }
     }
 }
