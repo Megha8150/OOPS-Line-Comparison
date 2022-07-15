9ef02367cf45e7f32fd4d7bf2166ede807d201c1
Megha8150 <meghananayak436@gmail.com>
2022-07-16 00:28:26 +0530

    Calculate length1 and length2

--- LineComparisonOOPS/Program.cs
+++ LineComparisonOOPS/Program.cs
@@ -2,6 +2,10 @@
 using LineComparisonOOPS;
 //LineComparison.lineComparison();
 
-LineComparison line = new LineComparison();
-double length1= line.length();
-Console.WriteLine(length1);
+LineComparison line1 = new LineComparison();
+LineComparison line2 = new LineComparison();
+
+double length1 = line1.length();
+double length2 = line2.length();
+Console.WriteLine("Length of line 1 is " +length1);
+Console.WriteLine("Length of line 2 is " +length2);
